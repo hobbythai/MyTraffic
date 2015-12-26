48818d7f59a101cb9fb104dfb9c4d888ec448d56
kally <kally.thailand@gmail.com>
2015-12-26 14:59:06 +0700

    set widget

--- app/src/main/java/com/hobbythai/android/mytraffic/MainActivity.java
+++ app/src/main/java/com/hobbythai/android/mytraffic/MainActivity.java
@@ -1,13 +1,64 @@
 package com.hobbythai.android.mytraffic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //bind widget
+        bindWidget();
+
+        //set widget
+        setWidget();
+
+        //list view
+        listViewControl();
+
+    }//on create
+
+
+    private void listViewControl() {
+
+    }//list view control
+
+    private void setWidget() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"OK", Toast.LENGTH_SHORT).show();
+
+                //sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.dog); //get base internal mem
+                buttonMediaPlayer.start();
+
+
+            }
+        });
+
+    }//set widget
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }//bind widget
+
+}//main
